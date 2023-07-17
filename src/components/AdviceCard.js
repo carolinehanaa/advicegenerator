45fd3114940ddf9abf3438da250c442896b42267
carolinehanaa <carolinehana28@gmail.com>
2023-07-17 12:44:07 -0700

    update

--- src/components/AdviceCard.js
+++ src/components/AdviceCard.js
@@ -3,19 +3,27 @@ import dividerDesktop from '../images/pattern-divider-desktop.svg'
 import dividerMobile from '../images/pattern-divider-mobile.svg'
 import dice from '../images/icon-dice.svg'
 import { FetchAdvice } from '../service/DataService'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function AdviceCard() {
     const [advice, setAdvice] = useState('');
     const [adviceNum, setAdviceNum] = useState('');
 
-    async function adviceData(){
-    let adviceFetch = await FetchAdvice();
-    console.log(adviceFetch);
+    async function adviceData() {
+      const maxAdviceNum = 217; // Maximum number of advice slips available
+      const randomAdviceNum = Math.floor(Math.random() * maxAdviceNum) + 1; // Generate a random number between 1 and maxAdviceNum
+      let adviceFetch = await FetchAdvice(randomAdviceNum);
+      console.log(adviceFetch);
+  
+      setAdvice(adviceFetch.slip.advice);
+      setAdviceNum(adviceFetch.slip.id)
+    }
+  
+    useEffect(() => {
+      adviceData();
+    }, []);
 
-    setAdvice(adviceFetch.slip.advice);
-    setAdviceNum(adviceFetch.slip.id)
-}
+
   return (
     <div>
         <div className='card'>
